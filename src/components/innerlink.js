d76fbae6e613c46939492efff14fe55d82647d7b
Ryan <ryanak@gmail.com>
2021-07-13 23:11:03 -0700

    change hp links

---
+++ src/components/innerlink.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+export default function Navlink (props) {
+  function showDetailBox(key, mainboxes) {
+    setTimeout(function(){
+    mainboxes[key].classList.add("d-none");
+    //document.querySelector(".detailbox").classList.remove("d-none");
+},500);
+} 
+
+function handleClick(theclass) {
+let mainboxes = document.querySelectorAll(".mainbox");
+for(let i = 0; i < mainboxes.length; i++) {
+    mainboxes[i].classList.add("animated","fadeOut");
+    showDetailBox(i,mainboxes);
+    
+}
+}
+
+var innerdata = [];
+
+props.myinfo.map((elem) => {
+  innerdata.header = elem.header;
+  innerdata.tagline = elem.tagline;
+  innerdata.class = elem.class;
+  return innerdata;
+});
+  
+  
+    return (
+      <span onClick={() => {props.setclass(props.currentClass); handleClick(innerdata.class) }}>{props.title}</span>
+    );
+  }
+
